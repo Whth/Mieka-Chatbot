1cab3055c2503071a5d98b3f7eab6af8bebba54a
Whth <88489697+Whth@users.noreply.github.com>
2023-09-02 14:13:58 +0800

    feat(adc plugin): base class which new plugin must inherit
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

---
+++ modules/plugin_base.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from typing import final
+
+from graia.ariadne import Ariadne
+
+
+class AbstractPlugin(ABC):
+    """
+    Abstract plugin class
+    """
+
+    @final
+    def __init__(self,
+                 ariadne_app: Ariadne):
+        self._ariadne_app: Ariadne = ariadne_app
+
+    @classmethod
+    @abstractmethod
+    def get_plugin_name(cls) -> str:
+        """
+        Get the plugin name
+        :return:
+        :rtype:
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_plugin_description(cls) -> str:
+        """
+        Get the plugin description
+        :return:
+        :rtype:
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_plugin_version(cls) -> str:
+        """
+        Get the plugin version
+        :return:
+        :rtype:
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_plugin_author(cls) -> str:
+        """
+        Get the plugin author
+        :return:
+        :rtype:
+        """
+        pass
+
+    @abstractmethod
+    def install(self):
+        """
+        Install the plugin
+        """
+        pass
