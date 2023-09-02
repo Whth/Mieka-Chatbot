822c509bc7b0fba784bebf9cfeaba0e94947fbb2
Whth <88489697+Whth@users.noreply.github.com>
2023-09-02 21:30:22 +0800

    feat(extension installation imp):
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- chat_bot.py
+++ chat_bot.py
@@ -1,19 +1,27 @@
 import os
 from importlib import import_module
+from types import MappingProxyType
 from typing import Optional, Dict, Type, Sequence, List
 
 from graia.ariadne.app import Ariadne
 from graia.ariadne.connection.config import WebsocketClientConfig
 from graia.ariadne.entry import config
 
-from constant import MAIN
+from constant import MAIN, EXTENSION_DIR
 from modules.plugin_base import AbstractPlugin
 
 
-def list_directories(directory):
-    abs_directory = os.path.abspath(directory)
-    directories = [os.path.join(abs_directory, name) for name in os.listdir(abs_directory) if os.path.isdir(os.path.join(abs_directory, name))]
-    return directories
+def get_all_sub_dirs(directory:str)->List[str]:
+    """
+     
+    Args:
+        directory (str):
+
+    Returns:
+
+    """
+    subdirectories = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
+    return subdirectories
 
 class ChatBot(object):
     """
@@ -33,32 +41,59 @@ class ChatBot(object):
 
         self._bot_name: str = bot_name
 
-        self._plugins: Dict[str, AbstractPlugin] = {}
+        self._installed_plugins: Dict[str, AbstractPlugin] = {}
 
-    def _detect_plugins(self,extension_path:str):
+    @property
+    def installed_plugins(self) -> MappingProxyType[str, AbstractPlugin]:
+        """
+        Installed plugins
+        Returns:
+
+        """
+        return MappingProxyType(self._installed_plugins)
+    def _install_all_extensions(self):
+        """
+        Install all extensions
+        Returns:
+
+        """
+        detected_plugins=self._detect_plugins(EXTENSION_DIR)
+        for plugin in detected_plugins:
+            self._install_plugin(plugin)
+
+    @staticmethod
+    def _detect_plugins(extensions_path:str):
         """
         Detect plugins in the extension path
-        :param extension_path:
-        :type extension_path:
+        :param extensions_path:
+        :type extensions_path:
         :return:
         :rtype:
         """
-        for file in os.listdir(extension_path):
+        sub_dirs:List[str] = get_all_sub_dirs(extensions_path)
+        detected_plugins:List[Type[AbstractPlugin]] = []
+        for sub_dir in sub_dirs:
+            extension_attr_chain:str =f'{extensions_path}.{sub_dir}'
+            detected_plugins.extend(import_plugin(extension_attr_chain))
+        return detected_plugins
 
 
-    def add_plugin(self, plugin: Type[AbstractPlugin]) -> None:
+    def _install_plugin(self, plugin: Type[AbstractPlugin]) -> None:
         """
-
+        This code snippet defines a method called _install_plugin that takes a plugin parameter of type AbstractPlugin.
+        It checks if the plugin is already registered and raises an error if it is.
+        Then, it creates an instance of the plugin and calls its install method.
+        Finally, it adds the plugin instance to a dictionary called _installed_plugins with the plugin name as the key.
         :param plugin:
         :type plugin:
         :return: None
         :rtype:
         """
-        if plugin.get_plugin_name in self._plugins:
+        if plugin.get_plugin_name in self._installed_plugins:
             raise ValueError("Plugin already registered")
         plugin_instance = plugin(self._ariadne_app)
         plugin_instance.install()
-        self._plugins[plugin.get_plugin_name()] = plugin_instance
+        self._installed_plugins[plugin.get_plugin_name()] = plugin_instance
 
     def run(self) -> None:
         """
@@ -76,11 +111,11 @@ class ChatBot(object):
 
 
 
-def import_plugin(extension_dir_path: str) -> Sequence[Type[AbstractPlugin]]:
+def import_plugin(extension_attr_chain: str) -> Sequence[Type[AbstractPlugin]]:
     """
     load the extension and return the plugins in it
-    :param extension_dir_path:
-    :type extension_dir_path:
+    :param extension_attr_chain:
+    :type extension_attr_chain:
     :return:
     :rtype:
 
@@ -88,7 +123,7 @@ def import_plugin(extension_dir_path: str) -> Sequence[Type[AbstractPlugin]]:
         one extension is allowed to contain multiple extensions
     """
 
-    module = import_module(MAIN, extension_dir_path)  # load extension
+    module = import_module(MAIN, extension_attr_chain)  # load extension
     plugins: List[Type[AbstractPlugin]] = []  # init yield list
 
     for plugin_name in module.__all__:
@@ -96,7 +131,7 @@ def import_plugin(extension_dir_path: str) -> Sequence[Type[AbstractPlugin]]:
         plugin_name: str
         if not hasattr(module, plugin_name):
             # attr check
-            raise ImportError(f'Plugin {plugin_name} not found in {extension_dir_path}')
+            raise ImportError(f'Plugin {plugin_name} not found in {extension_attr_chain}')
         plugin: Type = getattr(module, plugin_name)
         if issubclass(plugin, AbstractPlugin):
             # check if plugin is subclass of AbstractPlugin
--- constant.py
+++ constant.py
@@ -2,3 +2,4 @@ import os
 
 PACKAGE_ROOT: str = os.path.abspath(os.path.dirname(__file__))
 MAIN = '.main'
+EXTENSION_DIR:str='extensions'
