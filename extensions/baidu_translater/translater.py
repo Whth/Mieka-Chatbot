585044612caba9846500da22317e3dacd2ab50bb
Whth <88489697+Whth@users.noreply.github.com>
2023-09-03 09:48:21 +0800

    style(adjust format):
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/baidu_translater/translater.py
+++ extensions/baidu_translater/translater.py
@@ -8,15 +8,18 @@ import requests
 class Translater:
     appid, appkey, url = None, None, None
     # configPath = f'{os.root_path.dirname(os.getcwd())}/baidu_translater/chat_dict.json'
-    configPath='L:\pycharm projects\chatBotComponents\\baidu_translater\config.json'
+    configPath = "L:\pycharm projects\chatBotComponents\\baidu_translater\config.json"
+
     def __init__(self):
         assert self.load_secret() == 1
-        print(f"Loading as \n"
-              f"\tappid: {self.appid}\n"
-              f"\tappkey: {self.appkey}\n"
-              f"\turl: {self.url}\n"
-              f"################################\n"
-              f"################################\n")
+        print(
+            f"Loading as \n"
+            f"\tappid: {self.appid}\n"
+            f"\tappkey: {self.appkey}\n"
+            f"\turl: {self.url}\n"
+            f"################################\n"
+            f"################################\n"
+        )
 
     def generate_sign(self, q, salt=None):
         """
@@ -36,27 +39,31 @@ class Translater:
         if salt is None:
             salt = random.randint(32768, 65536)
             return_salt = True
-        sign_str = f'{self.appid}{q}{salt}{self.appkey}'
+        sign_str = f"{self.appid}{q}{salt}{self.appkey}"
         m = hashlib.md5()
-        m.update(sign_str.encode('utf-8'))
+        m.update(sign_str.encode("utf-8"))
         if return_salt:
             # 入参没有salt时，返回salt
             return m.hexdigest(), salt
         else:
             return m.hexdigest()
 
-    def load_secret(self, path: str=None):
+    def load_secret(self, path: str = None):
         if path is None:
             path = self.configPath
 
-        print(f'congfigPath: {path}')
+        print(f"congfigPath: {path}")
         with open(path) as f:
             dataPack = json.load(f)
-            self.appid, self.appkey, self.url = dataPack.get('appid'), dataPack.get('appkey'), dataPack.get('url')
-            print('loading.....')
+            self.appid, self.appkey, self.url = (
+                dataPack.get("appid"),
+                dataPack.get("appkey"),
+                dataPack.get("url"),
+            )
+            print("loading.....")
         return 1
 
-    def translate(self, to_lang: str, q: str, from_lang: str = 'auto'):
+    def translate(self, to_lang: str, q: str, from_lang: str = "auto"):
         """
 
         :param from_lang:
@@ -69,26 +76,30 @@ class Translater:
             assert type(from_lang) == str
             assert type(q) == str
         except AssertionError:
-            return '......'
+            return "......"
         sign, salt = self.generate_sign(q=q)
 
-        payload = {'appid': self.appid, 'q': q, 'from': from_lang, 'to': to_lang, 'salt': salt, 'sign': sign}
+        payload = {
+            "appid": self.appid,
+            "q": q,
+            "from": from_lang,
+            "to": to_lang,
+            "salt": salt,
+            "sign": sign,
+        }
 
         r = requests.post(self.url, params=payload)
         result = r.json()
 
         # Show response
-        trans_result = result.get('trans_result')[0]
+        trans_result = result.get("trans_result")[0]
 
-        return str(trans_result.get('dst'))
+        return str(trans_result.get("dst"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     t = Translater()
 
-    q = '你好'
+    q = "你好"
     for i in range(10):
-
-        print(t.translate(to_lang='en', q=input()))
-
-
+        print(t.translate(to_lang="en", q=input()))
