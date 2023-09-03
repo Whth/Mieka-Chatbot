717da7b2f99bc714432f7c73fad32f2010729415
Whth <88489697+Whth@users.noreply.github.com>
2023-09-03 19:47:05 +0800

    feat(files related methods):
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- modules/file_manager.py
+++ modules/file_manager.py
@@ -1,5 +1,9 @@
+import datetime
 import os
+import time
 from typing import List
+
+import requests
 
 
 def get_current_file_path() -> str:
@@ -54,3 +58,68 @@ def get_all_sub_dirs(directory: str) -> List[str]:
     """
     subdirectories = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
     return subdirectories
+
+
+def clean_files(folder_path, time_limit):
+    """
+    清理文件夹中超过指定时长的文件
+    :param folder_path: 文件夹路径
+    :param time_limit: 时长限制，单位为秒
+    :return: 清理文件个数与清理掉的文件所占的储存空间大小
+    """
+    clean_file_num = 0
+    clean_file_size = 0
+
+    files = os.listdir(folder_path)
+
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        modify_time = os.path.getmtime(file_path)
+        current_time = time.time()
+        file_time = current_time - modify_time
+
+        if file_time > time_limit:
+            file_size = os.path.getsize(file_path)
+            os.remove(file_path)
+            clean_file_num += 1
+            clean_file_size += file_size
+            print(f"清理文件：{file_path}，大小：{file_size/1024/1024} MB")
+
+    print(f"清理文件个数：{clean_file_num}，清理文件大小：{clean_file_size/1024/1024} MB")
+
+
+def download_image(url: str, save_dir: str) -> str:
+    """
+    Downloads an image from the given URL and saves it to the specified directory.
+
+    Args:
+        url (str): The URL of the image.
+        save_dir (str): The directory where the image should be saved.
+
+    Returns:
+        str: The path where the image is saved, or None if the download fails.
+    """
+    # Generate a unique image name based on the current timestamp
+    img_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+    print(f"Downloading image from: {url}")
+
+    # Send a GET request to download the image
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Create the save directory if it doesn't exist
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        # Construct the path where the image will be saved
+        path = os.path.join(save_dir, f"{img_name}.png")
+
+        # Write the image content to a file
+        with open(path, "wb") as f:
+            f.write(response.content)
+
+        print(f"Downloaded image successfully. Saved at: {path}")
+        return path
+
+    return None
