543267ffed95835234c20a630d4bdf3954cfa95c
Whth <88489697+Whth@users.noreply.github.com>
2023-09-09 23:47:23 +0800

    feat(support highres config):
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/sdDev/api.py
+++ extensions/sdDev/api.py
@@ -1,3 +1,7 @@
 API_PNG_INFO: str = "sdapi/v1/png-info"
 API_TXT2IMG: str = "sdapi/v1/txt2img"
 API_IMG2IMG: str = "sdapi/v1/img2img"
+INIT_IMAGES_KEY = "init_images"  # used in img2img payload making
+IMAGE_KEY = "image"  # used in png-info payload making
+IMAGES_KEY = "images"  # used in txt2img payload making
+PNG_INFO_KEY = "info"
