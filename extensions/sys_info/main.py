8248dc3607f4770c387f06ed43127ba2c9e1a81c
Whth <88489697+Whth@users.noreply.github.com>
2023-09-17 00:26:12 +0800

    feat(extensions): support get cmd help
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/sys_info/main.py
+++ extensions/sys_info/main.py
@@ -51,7 +51,7 @@ class SysInfo(AbstractPlugin):
 
         def get_all_cmd_info() -> str:
             temp_string = "Available CMD:\n"
-            for cmd in ConfigClient.all_available_cmd():
+            for cmd in ConfigClient.get_all_available_cmd():
                 temp_string += f"\t{cmd}\n"
             return temp_string
 
