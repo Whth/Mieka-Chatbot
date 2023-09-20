78a42170700da82c9029a8faad2fafec0ba4a3e4
Whth <88489697+Whth@users.noreply.github.com>
2023-09-20 21:27:29 +0800

    feat(extensions): add task registry
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/EasyPin/main.py
+++ extensions/EasyPin/main.py
@@ -12,6 +12,9 @@ class EasyPin(AbstractPlugin):
     __TASK_LIST_CMD = "list"
     __TASK_DELETE_CMD = "delete"
     __TASK_TEST_CMD = "abd"
+    __TASK_HELP_CMD = "help"
+
+    CONFIG_TASKS_SAVE_PATH = "tasks_save_path"
 
     def _get_config_parent_dir(self) -> str:
         return os.path.abspath(os.path.dirname(__file__))
@@ -33,18 +36,20 @@ class EasyPin(AbstractPlugin):
         return "whth"
 
     def __register_all_config(self):
-        pass
+        self._config_registry.register_config(
+            self.CONFIG_TASKS_SAVE_PATH, f"{self._get_config_parent_dir()}/cache/tasks.pkl"
+        )
 
     def install(self):
         from graia.scheduler import GraiaScheduler
         from graia.scheduler.timers import crontabify
         from graia.broadcast import Broadcast
-        from graia.ariadne.message.chain import MessageChain
         from graia.ariadne.model import Group, Friend
         from graia.ariadne.event.message import MessageEvent
         from graia.ariadne.message.parser.base import ContainKeyword
 
         from .analyze import Preprocessor, DEFAULT_PRESET, TO_DATETIME_PRESET, DATETIME_TO_CRONTAB_PRESET
+        from .task import TaskRegistry
 
         self.__register_all_config()
         self._config_registry.load_config()
@@ -54,18 +59,26 @@ class EasyPin(AbstractPlugin):
         to_datetime_processor = Preprocessor(TO_DATETIME_PRESET)
         datetime_to_crontab_processor = Preprocessor(DATETIME_TO_CRONTAB_PRESET)
         full_processor = Preprocessor(DEFAULT_PRESET)
-
-        def make_task(time_string: str, task_content: str):
-            async def _task():
-                pass
-
-            scheduler.schedule(crontabify(full_processor.process(time_string) + " *"), cancelable=True)(_task)
+        task_registry = TaskRegistry(self._config_registry.get_config(self.CONFIG_TASKS_SAVE_PATH))
 
         def _test_convert(string: str) -> str:
             return to_datetime_processor.process(string)
 
+        def _help() -> str:
+            cmds = [
+                self.__TASK_SET_CMD,
+                self.__TASK_LIST_CMD,
+                self.__TASK_DELETE_CMD,
+                self.__TASK_TEST_CMD,
+                self.__TASK_HELP_CMD,
+            ]
+            help_strings = ["用于设置任务，第一个参数为执行时间，第二个参数为任务名称，任务内容由引用的消息决定", "列举出所有的定时任务", "删除指定的任务", "时间字符串解释测试", "展示这条信息"]
+            stdout = "\n".join(f"{cmd} {help_string}" for cmd, help_string in zip(cmds, help_strings))
+            return stdout
+
         tree = {
             self.__TASK_CMD: {
+                self.__TASK_HELP_CMD: _help,
                 self.__TASK_SET_CMD: None,
                 self.__TASK_LIST_CMD: None,
                 self.__TASK_DELETE_CMD: None,
@@ -73,8 +86,14 @@ class EasyPin(AbstractPlugin):
             }
         }
 
-        self._cmd_client.register(tree)
+        self._cmd_client.register(tree, True)
 
         @broad_cast.receiver(MessageEvent, decorators=[ContainKeyword(self.__TASK_CMD)])
-        async def pin_opreator(sender: Union[Group, Friend], message: MessageChain):
-            pass
+        async def pin_opreator(sender: Union[Group, Friend], message: MessageEvent):
+            cmd_tokens = str(message.message_chain)
+
+            def make_task(time_string: str, task_content: str):
+                async def _task():
+                    pass
+
+            scheduler.schedule(crontabify(full_processor.process(time_string) + " *"), cancelable=True)(_task)
--- extensions/EasyPin/task.py
+++ extensions/EasyPin/task.py
@@ -1,15 +1,54 @@
-from graia.scheduler import SchedulerTask
+import pathlib
+import pickle
+from typing import List, Coroutine, Any, Callable
+
+Task = Callable[[], Coroutine[Any, Any, Any]]
+
+from .analyze import is_crontab_expired
 
 
 class ScheduledTask(object):
-    def __init__(self, task: SchedulerTask, task_name: str):
-        self._task: SchedulerTask = task
+    def __init__(self, task: Task, task_name: str, task_crontab: str):
+        self._task: Task = task
         self._task_name: str = task_name
+        self._task_crontab: str = task_crontab
 
     @property
-    def task(self):
+    def task(self) -> Task:
         return self._task
 
     @property
-    def task_name(self):
+    def task_name(self) -> str:
         return self._task_name
+
+    @property
+    def task_crontab(self) -> str:
+        return self._task_crontab
+
+
+class TaskRegistry(object):
+    def __init__(self, save_path: str):
+        self._tasks: List[ScheduledTask] = []
+        self._save_path: str = save_path
+        if pathlib.Path(save_path).exists():
+            self.load_tasks()
+
+    def register_task(self, task: Task, task_name: str, task_crontab: str):
+        self._tasks.append(ScheduledTask(task, task_name, task_crontab))
+
+    def remove_outdated_tasks(self):
+        Unexpired_tasks = []
+        for task in self._tasks:
+            if not is_crontab_expired(task.task_crontab):
+                Unexpired_tasks.append(task)
+        self._tasks = Unexpired_tasks
+
+    def load_tasks(self):
+        with open(self._save_path, "rb") as f:
+            self._tasks = pickle.load(f)
+        self.remove_outdated_tasks()
+
+    def save_tasks(self):
+        pathlib.Path(self._save_path).mkdir(parents=True, exist_ok=True)
+        with open(self._save_path, "wb") as f:
+            pickle.dump(self._tasks, f)
