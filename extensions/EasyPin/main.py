ab3546edbe93e19de85e0f251139bbe8ca4c9382
Whth <88489697+Whth@users.noreply.github.com>
2023-09-22 12:49:58 +0800

    fix(extensions): incorrect task scheuduling strategy
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/EasyPin/main.py
+++ extensions/EasyPin/main.py
@@ -8,7 +8,7 @@ __all__ = ["EasyPin"]
 
 class EasyPin(AbstractPlugin):
     __TASK_CMD = "task"
-    __TASK_SET_CMD = "set"
+    __TASK_SET_CMD = "new"
     __TASK_LIST_CMD = "list"
     __TASK_DELETE_CMD = "delete"
     __TASK_TEST_CMD = "abd"
@@ -37,7 +37,7 @@ class EasyPin(AbstractPlugin):
 
     def __register_all_config(self):
         self._config_registry.register_config(
-            self.CONFIG_TASKS_SAVE_PATH, f"{self._get_config_parent_dir()}/cache/tasks.pkl"
+            self.CONFIG_TASKS_SAVE_PATH, f"{self._get_config_parent_dir()}/cache/tasks.json"
         )
 
     def install(self):
@@ -46,11 +46,10 @@ class EasyPin(AbstractPlugin):
         from graia.broadcast import Broadcast
         from graia.ariadne.event.message import GroupMessage
         from graia.ariadne.message.parser.base import ContainKeyword
-        from graia.ariadne.message.element import Forward
 
         from graia.ariadne.model import Group
         from .analyze import Preprocessor, DEFAULT_PRESET, TO_DATETIME_PRESET, DATETIME_TO_CRONTAB_PRESET
-        from .task import TaskRegistry
+        from .task import TaskRegistry, ReminderTask, T_TASK
 
         self.__register_all_config()
         self._config_registry.load_config()
@@ -60,7 +59,7 @@ class EasyPin(AbstractPlugin):
         to_datetime_processor = Preprocessor(TO_DATETIME_PRESET)
         datetime_to_crontab_processor = Preprocessor(DATETIME_TO_CRONTAB_PRESET)
         full_processor = Preprocessor(DEFAULT_PRESET)
-        task_registry = TaskRegistry(self._config_registry.get_config(self.CONFIG_TASKS_SAVE_PATH))
+        task_registry = TaskRegistry(self._config_registry.get_config(self.CONFIG_TASKS_SAVE_PATH), ReminderTask)
 
         def _test_convert(string: str) -> str:
             return to_datetime_processor.process(string)
@@ -78,7 +77,12 @@ class EasyPin(AbstractPlugin):
             return stdout
 
         def _task_list() -> str:
-            return "\n".join(f"{task.task_name} | {task.task_crontab}" for task in task_registry.tasks)
+            temp_string = "Task List:\n"
+            for crontab, tasks in task_registry.tasks.items():
+                for name, _task in tasks.items():
+                    _task: T_TASK
+                    temp_string += f"{_task.task_name} | {_task.crontab}\n"
+            return temp_string
 
         tree = {
             self.__TASK_CMD: {
@@ -92,9 +96,12 @@ class EasyPin(AbstractPlugin):
 
         self._cmd_client.register(tree, True)
 
-        # for task in task_registry.tasks:
-        #     scheduler.schedule(crontabify(task.task_crontab))(task.task)
-        # TODO currently not support serialize to disk
+        for task_name, tasks in task_registry.tasks.items():
+            for task in tasks.values():
+                task: T_TASK
+
+                scheduler.schedule(crontabify(task.crontab))(task.make(ariadne_app))
+
         @broad_cast.receiver(GroupMessage, decorators=[ContainKeyword(self.__TASK_CMD)])
         async def pin_opreator(group: Group, message: GroupMessage):
             pat = rf"{self.__TASK_CMD}\s+{self.__TASK_SET_CMD}\s+(\S+)(?:\s+(.+)|(?:\s+)?$)"
@@ -106,16 +113,15 @@ class EasyPin(AbstractPlugin):
             if not matches:
                 print("reg matches not accepted")
                 return
-            groups = matches[0]
+            match_groups = matches[0]
             origin_id = message.quote.id
 
-            async def _task():
-                msg_event = await ariadne_app.get_message_from_id(origin_id, group)
-                await ariadne_app.send_message(group, Forward(msg_event))
-                await ariadne_app.send_message(group, "不要忘了哦")
-
-            crontab = full_processor.process(groups[0], True) + " 0"
-            task_registry.register_task(task=_task, task_crontab=crontab, task_name=groups[1] if groups[1] else None)
-            print(f"Schedule task {task_registry.tasks[-1].task_name} with crontab {crontab}")
-            scheduler.schedule(crontabify(crontab))(_task)
+            crontab = full_processor.process(match_groups[0], True) + " 0"
+            task = ReminderTask(
+                crontab, [origin_id], target=group.id, task_name=match_groups[1] if match_groups[1] else None
+            )
+            task_registry.register_task(task=task)
+            task_registry.save_tasks()
+            await ariadne_app.send_message(group, f"Schedule new task:\n {task.task_name} | {crontab}")
+            scheduler.schedule(crontabify(crontab))(task.make(ariadne_app))
             await scheduler.schedule_tasks[-1].run()
