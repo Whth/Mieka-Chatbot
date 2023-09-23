013fea1ffe9a9a85ae58ba403713b8afcb4b4e18
Whth <88489697+Whth@users.noreply.github.com>
2023-09-23 22:10:12 +0800

    feat(extension): support batch send
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/pic-eval/img_manager.py
+++ extensions/pic-eval/img_manager.py
@@ -20,14 +20,44 @@ class ImageRegistry(object):  # TODO use other high performance serialize toolch
 
     @property
     def images_registry(self) -> MappingProxyType:
+        """
+        Returns the mapping proxy object for the images registry.
+
+        :return: The mapping proxy object for the images registry.
+        :rtype: MappingProxyType
+        """
         return self._images_registry_proxy
 
     def register(self, key: int, image_path: str) -> None:
+        """
+        Register an image in the registry.
+
+        Args:
+            key (int): The key associated with the image.
+            image_path (str): The path of the image.
+
+        Returns:
+            None: This function does not return anything.
+        """
         self._images_registry[key] = [image_path, time.time()]
         self.prune()
         self.save()
 
     def prune(self) -> None:
+        """
+        Prunes the images registry if the maximum size is exceeded.
+
+        This method checks if the maximum size of the images registry is set and if the number
+        of images in the registry is greater than the maximum size. If this condition is true,
+        it removes the oldest images from the registry until the size is reduced to the maximum
+        size.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         if self._max_size and len(self._images_registry) > self._max_size:
             num_to_remove = len(self._images_registry) - self._max_size
             for _ in range(num_to_remove):
@@ -37,6 +67,16 @@ class ImageRegistry(object):  # TODO use other high performance serialize toolch
         return self._images_registry[key][0]
 
     def remove(self, key: int, save: bool = False) -> bool:
+        """
+        Remove an image from the registry.
+
+        Args:
+            key (int): The key of the image to remove.
+            save (bool, optional): Indicates whether to save the changes to the registry. Defaults to False.
+
+        Returns:
+            bool: True if the image was successfully removed, False otherwise.
+        """
         if key in self._images_registry:
             file_path = self._images_registry[key][0]
             if self._recycle_folder:
