f8f92ab3046e96c719cd3a4283a2ac0441c66661
Whth <88489697+Whth@users.noreply.github.com>
2023-10-06 16:53:14 +0800

    feat(auth): save after most auth related opearation
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- modules/auth/core.py
+++ modules/auth/core.py
@@ -1,6 +1,8 @@
+import inspect
 import pathlib
+from functools import wraps
 from pydantic import PrivateAttr, validator
-from typing import Optional, NamedTuple, Any, Dict, Iterable, List
+from typing import Optional, NamedTuple, Any, Dict, Iterable, List, Callable, Type
 
 from .permissions import Permission, PermissionCode, PermissionManager
 from .resources import ResourceManager, Resource, required_perm_generator, RequiredPermission
@@ -12,6 +14,54 @@ from .utils import AuthBaseModel, make_label, UniqueLabel
 class Root(NamedTuple):
     id: int = 0
     name: str = "root"
+
+
+def final_handler(final_method: str, exception: Optional[Type[Exception]] = Exception) -> Callable:
+    """
+    Decorator that wraps a function and handles exceptions and a final method call.
+
+    Args:
+        final_method (str): The name of the final method to be called after the function is executed.
+        exception (Optional[Type[Exception]], optional): The type of exception to catch. Defaults to Exception.
+
+    Returns:
+        Callable: The wrapped function.
+
+    """
+
+    def decorator(func):
+        """
+        Decorates a function with the given decorator.
+
+        Parameters:
+            func (function): The function to be decorated.
+
+        Returns:
+            function: The decorated function.
+
+        Raises:
+            exception: If an exception occurs during the execution of the decorated function.
+
+        """
+        sig = inspect.signature(func)
+        if sig.return_annotation != bool:
+            raise ValueError("The return annotation of the decorated function must be bool.")
+
+        @wraps(func)
+        def wrapped(self, *args, **kwargs) -> bool:
+            result = False
+            try:
+                result = func(self, *args, **kwargs)
+            except exception:
+                pass
+            finally:
+                final_method_obj = getattr(self, final_method)
+                final_method_obj()
+            return result
+
+        return wrapped  # 更新装饰函数的元信息
+
+    return decorator
 
 
 class AuthorizationManager(AuthBaseModel):
@@ -49,51 +99,43 @@ class AuthorizationManager(AuthBaseModel):
         su_user = User(id=self.id, name=self.name, roles=[su_role])
         self._users.add_object(su_user)
 
+    @final_handler("save", KeyError)
     def add_user(self, user_id: int, user_name: str, user_roles: Optional[Iterable[UniqueLabel]] = None) -> bool:
-        try:
-            if user_roles:
-                user_roles: List[Role] = [self._roles.object_dict[label] for label in user_roles]
-            else:
-                user_roles = []
-            return self._users.add_object(User(id=user_id, name=user_name, roles=user_roles))
-        except KeyError:
-            return False
+        if user_roles:
+            user_roles: List[Role] = [self._roles.object_dict[label] for label in user_roles]
+        else:
+            user_roles = []
+        return self._users.add_object(User(id=user_id, name=user_name, roles=user_roles))
 
+    @final_handler("save", KeyError)
     def remove_user(self, user_id: int, user_name: str) -> bool:
-        try:
-            self._users.remove_object(make_label(user_id, user_name))
-            return True
-        except KeyError:
-            return False
+        self._users.remove_object(make_label(user_id, user_name))
+        return True
 
+    @final_handler("save", KeyError)
     def add_role(self, role_id: int, role_name: str, role_perms: Optional[Iterable[UniqueLabel]] = None) -> bool:
-        try:
-            if role_perms:
-                role_perms: List[Permission] = [self._permissions.object_dict[label] for label in role_perms]
-            else:
-                role_perms = []
+        if role_perms:
+            role_perms: List[Permission] = [self._permissions.object_dict[label] for label in role_perms]
+        else:
+            role_perms = []
 
-            return self._roles.add_object(Role(id=role_id, name=role_name, permissions=role_perms))
-        except KeyError:
-            return False
+        return self._roles.add_object(Role(id=role_id, name=role_name, permissions=role_perms))
 
+    @final_handler("save", KeyError)
     def remove_role(self, role_id: int, role_name: str) -> bool:
-        try:
-            self._roles.remove_object(make_label(role_id, role_name))
-            return True
-        except KeyError:
-            return False
+        self._roles.remove_object(make_label(role_id, role_name))
+        return True
 
+    @final_handler("save", KeyError)
     def add_perm(self, perm_id: int, perm_name: str) -> bool:
         return self._permissions.add_object(Permission(id=perm_id, name=perm_name))
 
+    @final_handler("save", KeyError)
     def remove_perm(self, perm_id: int, perm_name: str) -> bool:
-        try:
-            self._permissions.remove_object(make_label(perm_id, perm_name))
-            return True
-        except KeyError:
-            return False
+        self._permissions.remove_object(make_label(perm_id, perm_name))
+        return True
 
+    @final_handler("save", KeyError)
     def add_resource(self, resource_id: int, resource_name: str, source: Any, std_init: bool = True) -> bool:
         """
         Adds a resource to the object's resource collection.
@@ -137,35 +179,27 @@ class AuthorizationManager(AuthBaseModel):
                 )
             )
 
+    @final_handler("save", KeyError)
     def remove_resource(self, resource_id: int, resource_name: str) -> bool:
-        try:
-            self._resources.remove_object(make_label(id=resource_id, name=resource_name))
-            return True
-        except KeyError:
-            return False
+        self._resources.remove_object(make_label(id=resource_id, name=resource_name))
+        return True
 
+    @final_handler("save", KeyError)
     def grant_perm_to_resource(self, perm_label: str, resource_label: str, category_name: str) -> bool:
-        try:
-            source: Resource = self._resources.object_dict[resource_label]
-            perm_to_grant: Permission = self._permissions.object_dict[perm_label]
-            source.required_permissions.add_permission(perm_to_grant, category_name)
-            return True
-        except KeyError:
-            return False
+        source: Resource = self._resources.object_dict[resource_label]
+        perm_to_grant: Permission = self._permissions.object_dict[perm_label]
+        source.required_permissions.add_permission(perm_to_grant, category_name)
+        return True
 
+    @final_handler("save", KeyError)
     def grant_perm_to_role(self, perm_label: str, role_label: str) -> bool:
-        try:
-            self._roles.object_dict.get(role_label).add_permission(self._permissions.object_dict.get(perm_label))
-            return True
-        except KeyError:
-            return False
+        self._roles.object_dict.get(role_label).add_permission(self._permissions.object_dict.get(perm_label))
+        return True
 
+    @final_handler("save", KeyError)
     def grant_role_to_user(self, role_label: str, user_label: str) -> bool:
-        try:
-            self._users.object_dict.get(user_label).add_role(self._roles.object_dict.get(role_label))
-            return True
-        except KeyError:
-            return False
+        self._users.object_dict.get(user_label).add_role(self._roles.object_dict.get(role_label))
+        return True
 
     def save(self) -> None:
         self._permissions.save_object_list()
