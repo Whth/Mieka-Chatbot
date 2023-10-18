3eb02d3f19bf8f4ec653ed341414ee1cb46f9796
Whth <88489697+Whth@users.noreply.github.com>
2023-10-18 15:31:13 +0800

    feat(core): add user bat path to the constant
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- constant.py
+++ constant.py
@@ -7,6 +7,6 @@ CONFIG_DIR: str = "config"
 EXTENSION_DIR: str = "extensions"
 CONFIG_FILE_NAME: str = "config.json"
 REQUIREMENTS_FILE_NAME: str = "requirements.txt"
-
+USER_BATCH_SCRIPT_PATH = str(pathlib.Path(f"{ROOT}/user.bat"))
 Value = Union[str, int, float, List, Dict, bool]
 CONFIG_PATH_PATTERN = r"[\/]"
