3da6b6d7a7d90326977cd1065f083d1d08eba49f
Whth <88489697+Whth@users.noreply.github.com>
2023-10-29 15:00:04 +0800

    refactor(extension): add shared
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

---
+++ modules/shared.py
@@ -0,0 +1,49 @@
+from .auth.core import Permission, Resource, User, RequiredPermission, Role, PermissionCode, required_perm_generator
+from .cmd import NameSpaceNode, ExecutableNode, CmdBuilder
+from .config_utils import ConfigRegistry
+from .file_manager import (
+    download_file,
+    get_pwd,
+    explore_folder,
+    get_all_sub_dirs,
+    generate_random_string,
+    clean_files,
+    compress_image_max_res,
+    compress_image_max_vol,
+    get_current_file_path,
+    is_image,
+    rename_image_with_hash,
+    img_to_base64,
+    base64_to_img,
+    ContentCacher,
+)
+from .plugin_base import AbstractPlugin
+
+__all__ = [
+    "AbstractPlugin",
+    "Permission",
+    "PermissionCode",
+    "required_perm_generator",
+    "Resource",
+    "User",
+    "RequiredPermission",
+    "Role",
+    "NameSpaceNode",
+    "ExecutableNode",
+    "CmdBuilder",
+    "ConfigRegistry",
+    "download_file",
+    "get_pwd",
+    "explore_folder",
+    "get_all_sub_dirs",
+    "generate_random_string",
+    "clean_files",
+    "compress_image_max_res",
+    "compress_image_max_vol",
+    "get_current_file_path",
+    "is_image",
+    "rename_image_with_hash",
+    "img_to_base64",
+    "base64_to_img",
+    "ContentCacher",
+]
