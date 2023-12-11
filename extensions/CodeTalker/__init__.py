46512631d38f6269f5c893390ccb64923878a985
Whth <88489697+Whth@users.noreply.github.com>
2023-12-11 22:26:27 +0800

    refactor(extension): use new impl
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- extensions/CodeTalker/__init__.py
+++ extensions/CodeTalker/__init__.py
@@ -5,22 +5,20 @@ from typing import List
 
 from sparkdesk_api.core import SparkAPI
 
-from modules.shared import (
-    get_pwd,
-    AbstractPlugin,
-    ExecutableNode,
-    Permission,
-    PermissionCode,
-    required_perm_generator,
-    RequiredPermission,
-)
+from modules.shared import get_pwd, AbstractPlugin, ExecutableNode, EnumCMD, CmdBuilder, NameSpaceNode
 from .fuzzy import FuzzyDictionary
 
 __all__ = ["CodeTalker"]
 
 
-class CMD:
-    CHAT: str = "chat"
+class CMD(EnumCMD):
+    chat = ["ct", "fed"]
+    chatcfg = ["ctc", "ctcfg"]
+    list = ["l", "ls"]
+    set = ["s", "st"]
+    cache = ["c", "cac"]
+    clear = ["cls", "clr"]
+    size = ["s", "sz"]
 
 
 class CodeTalker(AbstractPlugin):
@@ -33,16 +31,25 @@ class CodeTalker(AbstractPlugin):
     CONFIG_DICTIONARY_PATH = "dictionary_path"
     CONFIG_RE_GENERATE_PROBABILITY = "re_generate_probability"
     CONFIG_MAX_TOKENS = "max_tokens"
-
+    CONFIG_SILENT_FEEDBACK = "silent_feedback"
     DefaultConfig = {
         CONFIG_SECRETS_APPID: "your appid",
         CONFIG_SECRETS_APIKEY: "your api key",
         CONFIG_SECRETS_API_SECRETS: "your api secret",
         CONFIG_API_VERSION: 2.1,
-        CONFIG_PRE_APPEND_HISTORY: [],
         CONFIG_DICTIONARY_PATH: f"{get_pwd()}/fuzzy_dictionary.json",
         CONFIG_RE_GENERATE_PROBABILITY: 0.3,
         CONFIG_MAX_TOKENS: 150,
+        CONFIG_PRE_APPEND_HISTORY: [],
+        CONFIG_SILENT_FEEDBACK: [
+            "I'm sorry, I don't understand.",
+            "I'm sorry, I can't help you.",
+            "I don't have an answer for that.",
+            "I don't know what you mean.",
+            "I don't understand.",
+            "I can't help you.",
+            "NG",
+        ],
     }
 
     @classmethod
@@ -51,11 +58,11 @@ class CodeTalker(AbstractPlugin):
 
     @classmethod
     def get_plugin_description(cls) -> str:
-        return "use Spark 2.0 api"
+        return "use Spark api"
 
     @classmethod
     def get_plugin_version(cls) -> str:
-        return "0.0.4"
+        return "0.0.5"
 
     @classmethod
     def get_plugin_author(cls) -> str:
@@ -75,19 +82,34 @@ class CodeTalker(AbstractPlugin):
         fuzzy_dictionary = FuzzyDictionary(save_path=self._config_registry.get_config(self.CONFIG_DICTIONARY_PATH))
         print(f"Loading Fuzzy Dictionary Size:{len(fuzzy_dictionary.dictionary.keys())}")
         history = copy.deepcopy(self._config_registry.get_config(self.CONFIG_PRE_APPEND_HISTORY))
-        su_perm = Permission(id=PermissionCode.SuperPermission.value, name=self.get_plugin_name())
-        req_perm: RequiredPermission = required_perm_generator(
-            target_resource_name=self.get_plugin_name(), super_permissions=[su_perm]
+
+        configurable = {
+            self.CONFIG_API_VERSION,
+            self.CONFIG_RE_GENERATE_PROBABILITY,
+            self.CONFIG_MAX_TOKENS,
+        }
+        builder = CmdBuilder(
+            config_getter=self.config_registry.get_config, config_setter=self.config_registry.set_config
         )
 
-        def _talk(message: str, max_tokens: int = 0) -> str:
+        def _talk(*message_token: str) -> str:
+            """
+            Executes the _talk function to generate a response based on the given message tokens.
+
+            Args:
+                *message_token (str): The message tokens to be processed.
+
+            Returns:
+                str: The generated response based on the message tokens.
+            """
+            message = " ".join(message_token)
             # Search for similar words in the fuzzy dictionary
             search: List[str] = fuzzy_dictionary.search(message)
 
             # If the random number is less than or equal to the re-generate probability
             # or no similar words are found in the dictionary
             if random.random() <= self._config_registry.get_config(self.CONFIG_RE_GENERATE_PROBABILITY) or not search:
-                max_tokens = max_tokens if max_tokens else self._config_registry.get_config(self.CONFIG_MAX_TOKENS)
+                max_tokens = self._config_registry.get_config(self.CONFIG_MAX_TOKENS)
                 # Generate a response using the Spark API
                 response: str = self.sparkAPI.chat(
                     query=message,
@@ -99,7 +121,7 @@ class CodeTalker(AbstractPlugin):
                     fuzzy_dictionary.register_key_value(message, response)
                     fuzzy_dictionary.save_to_json()
                 else:
-                    response = "a"
+                    response = random.choice(self.config_registry.get_config(self.CONFIG_SILENT_FEEDBACK))
                 print(f"Request Response: {response}")
             else:
                 # Select a random response from the search results
@@ -108,15 +130,51 @@ class CodeTalker(AbstractPlugin):
             return response
 
         tree = ExecutableNode(
-            name=CMD.CHAT,
-            required_permissions=req_perm,
+            **CMD.chat.export(),
+            required_permissions=self.required_permission,
             source=_talk,
-            help_message=f"{self.get_plugin_name()} - {self.get_plugin_description()}",
         )
-        self._auth_manager.add_perm_from_req(req_perm)
-        self._root_namespace_node.add_node(tree)
+
+        self.root_namespace_node.add_node(tree)
+
+        config_tree = NameSpaceNode(
+            **CMD.chatcfg.export(),
+            required_permissions=self.required_permission,
+            help_message="Config for CodeTalker",
+            children_node=[
+                ExecutableNode(**CMD.set.export(), source=builder.build_group_setter_for(configurable)),
+                ExecutableNode(**CMD.list.export(), source=builder.build_list_out_for(configurable)),
+                NameSpaceNode(
+                    **CMD.cache.export(),
+                    help_message="CodeTalker cache utils",
+                    children_node=[
+                        ExecutableNode(
+                            **CMD.clear.export(),
+                            source=lambda: fuzzy_dictionary.dictionary.clear(),
+                            help_message="Clear CodeTalker cache",
+                        ),
+                        ExecutableNode(
+                            **CMD.size.export(),
+                            source=lambda: f"CodeTalker cache size: {len(fuzzy_dictionary.dictionary)}",
+                            help_message="Get CodeTalker cache size",
+                        ),
+                    ],
+                ),
+            ],
+        )
+        self.root_namespace_node.add_node(config_tree)
 
     def chat(self, string: str) -> str:
+        """
+        Performs a chat operation using the SparkAPI.
+
+        Args:
+            string (str): The input string for the chat operation.
+
+        Returns:
+            str: The output generated by the chat operation.
+
+        """
         spark_par = partial(
             SparkAPI,
             app_id=self._config_registry.get_config(self.CONFIG_SECRETS_APPID),
