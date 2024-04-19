d3c5210e5d8d0d28e44ca3c4ac714efebe7e5f8e
Whth <88489697+Whth@users.noreply.github.com>
2024-04-19 11:00:28 +0800

    ci(version): bump to 0.5.1
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -18,7 +18,7 @@ class DefaultConfig(Enum):
     VERIFY_KEY = "INITKEYXBVCdNG0"
     ACCOUNT_ID = 1234567890
     ACCEPTED_MESSAGE_TYPES = ["GroupMessage"]
-    VERSION = "v0.5.0"
+    VERSION = "v0.5.1"
 
 
 def make_help_cmd(client: NameSpaceNode):
